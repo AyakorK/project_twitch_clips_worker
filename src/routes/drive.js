b247bf8b1235086662e77bbb87e4e46246a83b37
AyakorK <guillaume.moret@yahoo.com>
2026-07-04 21:32:33 +0200

    fix: Add logs

--- src/routes/drive.js
+++ src/routes/drive.js
@@ -165,6 +165,8 @@ router.post('/import-folder', express.json(), checkQuota, (req, res) => {
 
     const send = (data) => res.write(`data: ${JSON.stringify(data)}\n\n`);
 
+    send({ type: 'log', message `Lancement gdown: ${url} vers ${destPath}` });
+
     const gdown = spawn('/home/clip-worker/venv/bin/gdown', [
 
         '--folder', url, '-O', destPath,
